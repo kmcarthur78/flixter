2399cc70e4a8e9629d0c09348b49e81fcb511233
Kelsey McArthur <kelseymcarthur@Kelseys-MacBook-Pro.local>
2019-01-28 18:42:46 -0500

    Second commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Kelsey McArthur on 1/22/19.
+//  Copyright © 2019 KMcArthur7878. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
