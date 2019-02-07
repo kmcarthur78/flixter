edeb05e5a185c7406d7bca4bc9cc2d567b6be8a1
Kelsey McArthur <kelseymcarthur@kelseys-macbook-pro.local>
2019-02-07 17:42:24 -0500

    Added Superhero Movie Grid

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Kelsey McArthur on 2/6/19.
+//  Copyright © 2019 KMcArthur7878. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- flixter/MoviesViewController.swift
+++ flixter/MoviesViewController.swift
@@ -65,17 +65,29 @@ class MoviesViewController: UIViewController, UITableViewDataSource,UITableViewD
         let posterUrl = URL(string: baseUrl + posterPath)
         
         cell.posterView.af_setImage(withURL: posterUrl!)
+        
         return cell
     }
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        //find selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+    
 
 }
